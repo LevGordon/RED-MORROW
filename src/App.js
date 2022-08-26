0a7ae1249e86a612dcea6de3f63cd7ce324ccd66
Lev_Gordon <lev.gordonsa@gmail.com>
2022-08-25 19:31:38 -0700

    uploaded some photos and updated the default text as a placeholder

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
-import logo from './logo.svg';
+import rmLogoColor from './photos/rmLogoColor.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={rmLogoColor} className="App-logo" alt="Red Morrow logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Welcome to the beggining of the Red Morrow Website.
         </p>
+        <p>Follow us on instagram </p> 
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.instagram.com/redmorrowband/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          @redmorrowband
         </a>
       </header>
     </div>
