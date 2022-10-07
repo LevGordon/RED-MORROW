9968b3003279d03969947742caea5ad969854a0f
Lev_Gordon <lev.gordonsa@gmail.com>
2022-10-06 19:39:38 -0700

    basic mobile responsiveness

--- src/Home/NavBar.js
+++ src/Home/NavBar.js
@@ -1,32 +1,78 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import rmLogoColor from "../photos/rmLogoColor.png";
+
+function useWindowSize() {
+  const [size, setSize] = useState([window.innerHeight, window.innerWidth]);
+  useEffect(() => {
+    const handleResize = () => {
+      setSize([window.innerHeight, window.innerWidth]);
+    };
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+  return size;
+}
 
 function NavBar() {
-  return (
-    <div className="container">
-      <div className="row-6">
-        <div className="col">
-          <h1>Logo</h1>
+  const [height, width] = useWindowSize();
+
+
+  const desktopNavBar = (
+    <div className="navbar-container">
+      <div className="navbar-left">
+        <div className="navbar-menu">
+          <button>Menu</button>
         </div>
-        <div className="col">
-          <h1>Menu</h1>
+        <div className="navbar-logo">
+          <img
+            src={rmLogoColor}
+            alt="Red Morrow Official Logo"
+            className="navbar-rm-logo-img"
+          />
         </div>
       </div>
-      <div className="row-6">
-        <div className="col">
-          <h1>Music</h1>
+      <div className="navbar-right">
+        <div className="navbar-music">
+          <button>Music</button>
         </div>
-        <div className="col">
-          <h1>Merch</h1>
+        <div className="navbar-merch">
+          <button>Merch</button>
         </div>
-        <div className="col">
-          <h1>Contact</h1>
+        <div className="navbar-contact">
+          <button>Contact</button>
         </div>
-        <div className="col">
-          <h1>About Us</h1>
+        <div className="navbar-about-us">
+          <button>About Us</button>
         </div>
       </div>
     </div>
   );
+
+  const mobileNavBar = (
+    <div className="navbar-container">
+      <div className="navbar-left">
+        <div className="navbar-logo">
+          <img
+            src={rmLogoColor}
+            alt="Red Morrow Official Logo"
+            className="navbar-rm-logo-img"
+          />
+        </div>
+      </div>
+      <div className="navbar-right">
+        <div className="navbar-music">
+          <button>Music</button>
+        </div>
+        <div className="navbar-menu">
+          <button>Menu</button>
+        </div>
+      </div>
+    </div>
+  )
+
+  return width > 768 ? desktopNavBar : mobileNavBar;
 }
 
 export default NavBar;
