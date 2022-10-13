005a27ac9d09e3f25dba7de9ec71d4bf93fd7c6e
Lev_Gordon <lev.gordonsa@gmail.com>
2022-10-13 13:30:28 -0700

    basic /contact page

--- src/Pages/Contact.js
+++ src/Pages/Contact.js
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from "react";
 
 function Contact() {
   return (
-    <div>Contact</div>
-  )
+    <div className="contact-main-container">
+      <div className="contact-text-box">
+        <h2> You can contact us in two ways:</h2>
+
+      </div>
+      <div className="contact-two-blocks">
+        <div className="contact-gmail-block">
+          <img src="../photos/gmail-logo.png" alt="Contact Red Morrow Gmail"/>
+          <p>redmorrowband@gmail.com</p>
+        </div>
+        <div className="contact-insta-block">
+          <img src="../photos/insta-logo.png" alt="Contact Red Morrow Instagram"/>
+          <a href="https://www.instagram.com/redmorrowband/">@redmorrowband</a>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Contact
+export default Contact;
