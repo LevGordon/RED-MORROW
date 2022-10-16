7a42e027f368c87b34f0b46ffc3c10cca60fb730
Lev_Gordon <lev.gordonsa@gmail.com>
2022-10-15 21:06:15 -0700

    styled emailJS addition

--- src/Pages/ContactEmailForm.js
+++ src/Pages/ContactEmailForm.js
@@ -1,5 +1,5 @@
-import React, { useRef } from 'react';
-import emailjs from '@emailjs/browser';
+import React, { useRef } from "react";
+import emailjs from "@emailjs/browser";
 
 const ContactUs = () => {
   const form = useRef();
@@ -7,25 +7,45 @@ const ContactUs = () => {
   const sendEmail = (e) => {
     e.preventDefault();
 
-    emailjs.sendForm('service_v83gg4k', 'template_8mc8yfr', form.current, 'L957q1-JRrU8r6VO8')
-      .then((result) => {
+    emailjs
+      .sendForm(
+        "service_v83gg4k",
+        "template_8mc8yfr",
+        form.current,
+        "L957q1-JRrU8r6VO8"
+      )
+      .then(
+        (result) => {
           console.log(result.text);
-      }, (error) => {
+        },
+        (error) => {
           console.log(error.text);
-      });
+        }
+      );
   };
 
-  return (
-    <form ref={form} onSubmit={sendEmail}>
-      <label>Name</label>
-      <input type="text" name="user_name" />
-      <label>Email</label>
-      <input type="email" name="user_email" />
-      <label>Message</label>
-      <textarea name="message" />
-      <input type="submit" value="Send" />
+
+  const contactForm = (
+    <form ref={form} onSubmit={sendEmail} className="contact-form">
+      <div className="contact-label-input-stack">
+        <label>Your Name</label>
+        <input type="text" name="user_name" />
+      </div>
+      <div className="contact-label-input-stack">
+        <label>Your Email</label>
+        <input type="email" name="user_email" />
+      </div>
+      <div className="contact-label-input-stack">
+        <label>Enter Message</label>
+        <textarea name="message" />
+      </div>
+      <div>
+      <button type="submit" className="contact-form-send-button">Send</button>
+      </div>
     </form>
   );
+
+  return contactForm
 };
 
-export default ContactUs
+export default ContactUs;
