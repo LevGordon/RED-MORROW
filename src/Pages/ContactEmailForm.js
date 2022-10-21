5e3b70bf531a4bd00ba0c465c24fe5fd5e59675d
Lev_Gordon <lev.gordonsa@gmail.com>
2022-10-20 19:46:39 -0700

    added form validation for empty fields for the contact form, made it adaptive to add features in the future

--- src/Pages/ContactEmailForm.js
+++ src/Pages/ContactEmailForm.js
@@ -10,8 +10,15 @@ const PUBLIC_KEY = process.env.REACT_APP_EMAILJS_PUBLIC_KEY
 
 
 const ContactUs = () => {
+  const [errorMessage, setErrorMessage] = useState()
+
   const [emailSent, setEmailSent] = useState(false)
   const form = useRef();
+
+
+  const nameRef = useRef();
+  const emailRef = useRef();
+  const messageRef = useRef();
 
   const sendEmail = (e) => {
     e.preventDefault();
@@ -35,45 +42,56 @@ const ContactUs = () => {
 
 
 
-  const contactFormValidation = () => {
+  const contactFormValidation = (e) => {
+    e.preventDefault()
+    setErrorMessage(null)
     
+    if (!nameRef.current.value || !emailRef.current.value || !messageRef.current.value) {
+      setErrorMessage("Please fill out all of the fields above!")
+    } else {
+      setTimeout(() => {
+        setEmailSent(true)
+        sendEmail(e)
+      }, 1000)
+    }
 
   }
 
 
-  const { transform, opacity } = useSpring({
+  const { transform } = useSpring({
     opacity: 1,
     transform: `perspective(600px) rotateY(${emailSent ? 180 : 0}deg)`,
     config: { mass: 5, tension: 500, friction: 80 },
   })
 
   const emailSentMessage = (
-    <a.div style={{opacity, transform}} className="contact-email-was-sent">
+    <a.div style={{ transform}} className="contact-email-was-sent">
         <h3 className="contact-success-h3">Your email was sent!</h3>
     </a.div>
   )
 
   const contactForm = (
-    <div style={{opacity,
+    <div style={{
       transform,
       rotateY: '180deg',}} className="contact-form" >
-      <form ref={form} onSubmit={sendEmail}>
+      <form ref={form} onSubmit={contactFormValidation}>
       <div className="contact-label-input-stack">
         <label className="contact-label">Your Name</label>
-        <input type="text" name="user_name" className="contact-input"/>
+        <input ref={nameRef} type="text" name="user_name" className="contact-input"/>
       </div>
       <div className="contact-label-input-stack">
         <label className="contact-label">Your Email</label>
-        <input type="email" name="user_email" className="contact-input"/>
+        <input ref={emailRef} type="email" name="user_email" className="contact-input"/>
       </div>
       <div className="contact-label-input-stack">
         <label className="contact-label">Enter Message</label>
-        <Textarea name="message" className="contact-textarea-message"/>
+        <Textarea ref={messageRef} name="message" className="contact-textarea-message"/>
       </div>
       <div className="contact-form-button-div">
-      <button type="submit" onClick={() => setTimeout(() => setEmailSent(true), 1000)} className="contact-form-send-button">Send</button>
+      <button type="submit" className="contact-form-send-button">Send</button>
       </div>
     </form>
+      <h5 className="error-message">{errorMessage}</h5>
 
     </div>
     
