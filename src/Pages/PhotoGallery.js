b17fd2b995435992effcf663dfe5cad7b2e75595
Lev_Gordon <lev.gordonsa@gmail.com>
2022-10-31 16:26:38 -0700

    bare structure of photo-gallery, need to be inspired for the work to be worthwhile

--- src/Pages/PhotoGallery.js
+++ src/Pages/PhotoGallery.js
@@ -1,9 +1,42 @@
 import React from 'react'
 
 function PhotoGallery() {
+
+  const events = [
+    {
+      name: '',
+      date: '',
+    },
+    {
+      name: '',
+      date: '',
+
+    },
+    {
+      name: '',
+      date: '',
+
+    },
+    {
+      name: '',
+      date: '',
+
+    }
+  ];
+
+
+
+
   return (
-    <div>
+    <div className="photo-gallery-container">
+      <div className='photo-gallery-top-half'>
         <h3>PhotoGallery</h3>
+        <button className='photo-gallery-random-browse-button'>Browse at Random!</button>
+      </div>
+      <div className='photo-gallery-bottom-half'>
+        
+        </div>
+       
     </div>
   )
 }
